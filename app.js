e67b0290d2024ac855d7f4ec813061394bf8723b
Fuad <fvkfuad@gmail.com>
2021-01-11 20:32:57 +0530

    modified store session in database

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use((req, res, next) => {
 //   next();
 // });
 var store = new MongoDBStore({
-  uri: 'mongodb://localhost:27017/connect_mongodb_session_test',
+  uri: process.env.MONGO_URL,
   collection: 'mySessions'
 });
 
