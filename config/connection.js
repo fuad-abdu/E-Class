bb435fd8fc8645cb7b1b7dd33996c10612f5dc92
Fuad <fvkfuad@gmail.com>
2021-01-12 15:13:54 +0530

    modified connection.js

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ module.exports.connect = function (done) {
     const dbname = 'ClassManagementSystem'
     const url = process.env.MONGO_URL || "mongodb://localhost:27017/ClassManagementSystem";
 
-    MongoClient.connect(url, (err, data) => {
+    MongoClient.connect(url, {useUnifiedTopology:true}, (err, data) => {
         if (err) return done(err)
         state.db = data.db(dbname)
         done()
