16bb320f6751c6f23930b328006ff24ead2e2681
Fuad <fvkfuad@gmail.com>
2021-01-13 21:04:45 +0530

    changed layouts , and change connection

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const state = {
 
 module.exports.connect = function (done) {
     const dbname = 'ClassManagementSystem'
-    const url = process.env.MONGO_URL || "mongodb://localhost:27017/ClassManagementSystem";
+    const url = "mongodb://localhost:27017/ClassManagementSystem";
 
     MongoClient.connect(url, {useUnifiedTopology:true}, (err, data) => {
         if (err) return done(err)
